badafb18ead025c4c96d935fa0bf3f43b2263216
Michael Fitzgerald <126652136+mjf1406@users.noreply.github.com>
2024-12-10 14:54:25 +0900

    Update route.ts

--- src/app/api/getTimetables/route.ts
+++ src/app/api/getTimetables/route.ts
@@ -6,7 +6,7 @@ import type { Timetable, Class, Slot, SlotClass } from '~/server/db/types';
 import { auth } from '@clerk/nextjs/server';
 
 export async function GET() {
-    const { userId } = await auth();
+    const { userId } = auth();
     if (!userId) {
         return NextResponse.json({ error: 'User not authenticated' }, { status: 401 });
     }
