98235fde6261a795c9d526369884605f3748846d
Michael J. Fitzgerald <michael.fitzgerald.1406@gmail.com>
2025-04-10 19:47:06 +0900

    fixed?

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,25 @@
-import { type Config } from "drizzle-kit";
-import { env } from "~/env";
+// import { type Config } from "drizzle-kit";
+// import { env } from "~/env";
 
-export default {
-  schema: "./src/server/db/schema.ts",
-  out: './drizzle',
-  dialect: "sqlite",
-  driver: "turso",
+// export default {
+//   schema: "./src/server/db/schema.ts",
+//   out: './drizzle',
+//   dialect: "sqlite",
+//   driver: "turso",
+//   dbCredentials: {
+//     url: env.TURSO_DATABASE_URL,
+//     authToken: env.TURSO_AUTH_TOKEN,
+//   },
+// } satisfies Config;
+
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  schema: './src/server/db/schema.ts',
+  out: './migrations',
+  dialect: 'turso',
   dbCredentials: {
-    url: env.TURSO_DATABASE_URL,
-    authToken: env.TURSO_AUTH_TOKEN,
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
   },
-} satisfies Config;
+});
